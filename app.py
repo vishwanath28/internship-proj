4d280387a1a862ae52de0410989a7e731308d7c6
vishwanath28 <70139325+vishwanath28@users.noreply.github.com>
2022-05-25 23:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import requests
 import os
 
-os.chdir("https://github.com/vishwanath28/internship-proj.git")
+os.chdir("C:\\Users\\shail\\Documents\\Untitled Folder")
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
